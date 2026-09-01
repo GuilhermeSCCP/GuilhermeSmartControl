53ec2d035f0d436cd085501d266dc82872b61a7c
GuilhermeSCCP <guiguimr2502@gmail.com>
2026-09-01 17:14:58 -0300

    Começando a fazer as modificações pelo JavaScript

---
+++ assets/js/dashboard.js
@@ -0,0 +1,42 @@
+const areaBotoes = document.querySelectorAll(".botao_info");
+
+areaBotoes.forEach((areaBotao) => {
+    const botao = document.createElement('button');
+    botao.classList.add("botaoInfo");
+    botao.type = "button";
+    botao.innerText = "Info";
+
+    areaBotao.appendChild(botao);
+})
+
+const botaoMonitoracao = document.querySelector('.botao_monitoracao')
+
+botaoMonitoracao.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
+
+    botaoMonitoracao.querySelector('.info-text').classList.toggle('info-text-active');
+})
+
+const botaoControle = document.querySelector('.botao_controle')
+
+botaoControle.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    console.log(event.target);
+
+    botaoControle.querySelector('.info-text').classList.toggle('info-text-active');
+})
+
+// botaoInfo.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//     console.log(event.target);
+
+//     alert("Botão pressionado!");
+// })
+
+// function toggleInfo(){
+
+// }
